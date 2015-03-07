f75d5db614fc7341e3b4465b614c06dceba3ffa4
Eric Martin <ericmark26@gmail.com>
2015-03-07 11:33:52 -0500

    Stuff

--- ThreadTableViewController.swift
+++ ThreadTableViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ThreadTableViewController: UIViewController {
-
-        
+class ThreadTableViewController: UIViewController, UITextViewDelegate {
+    
+    
     @IBOutlet weak var messageBox: UITextView!
+    @IBOutlet weak var tableView: UITableView!
+    var originalCenter = CGPoint()
     
     func viewDidAppear() {
         
@@ -26,19 +28,26 @@ class ThreadTableViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        originalCenter = self.view.center
+        self.tableView.tableFooterView = UIView(frame: CGRectZero)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     
-
+    func textViewDidBeginEditing(textView: UITextView) {
+//        let keyHeight : CGFloat =
+//        CGFloat height = [[notification.userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].height;
+        self.view.center = CGPointMake(self.originalCenter.x, self.originalCenter.y-216)
+        println("HEY")
+    }
+    
 }
