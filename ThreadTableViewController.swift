a4859901b48c947a84c45524dc48d057e0b4b235
freedee12 <TeddyDubno@Teddys-MacBook-Pro.local>
2015-03-07 12:45:47 -0500

    username and password

--- ThreadTableViewController.swift
+++ ThreadTableViewController.swift
@@ -15,18 +15,86 @@ class ThreadTableViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     var originalCenter = CGPoint()
     
-    func viewDidAppear() {
+    
+    override func viewDidAppear(animated: Bool) {
         
+        if PFUser.currentUser() == nil {
+            var loginAlert:UIAlertController = UIAlertController(title: "Sign Up / Login", message: "Please sign up or login", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            loginAlert.addTextFieldWithConfigurationHandler({
+                textfield in
+                textfield.placeholder = "Your username"
+            })
+            
+            loginAlert.addTextFieldWithConfigurationHandler({
+                textfield in
+                textfield.placeholder = "Your password"
+                textfield.secureTextEntry = true
+            })
+            
+            loginAlert.addAction(UIAlertAction(title: "Login", style: UIAlertActionStyle.Default, handler: {
+                alertAction in
+                let textFields:NSArray = loginAlert.textFields as AnyObject! as NSArray
+                let usernameTextfield:UITextField = textFields.objectAtIndex(0) as UITextField
+                let passwordTextfield:UITextField = textFields.objectAtIndex(1) as UITextField
+                
+                PFUser.logInWithUsernameInBackground(usernameTextfield.text, password: passwordTextfield.text){
+                    (user:PFUser!, error:NSError!)->Void in
+                    if user != nil{
+                        println("Login successfull")
+                    }else{
+                        println("Login failed")
+                    }
+                    
+                    
+                }
+                
+                
+                
+                
+            }))
+            
+            loginAlert.addAction(UIAlertAction(title: "Sign Up", style: UIAlertActionStyle.Default, handler: {
+                alertAction in
+                let textFields:NSArray = loginAlert.textFields as AnyObject! as NSArray
+                let usernameTextfield:UITextField = textFields.objectAtIndex(0) as UITextField
+                let passwordTextfield:UITextField = textFields.objectAtIndex(1) as UITextField
+                
+                var sweeter:PFUser = PFUser()
+                sweeter.username = usernameTextfield.text
+                sweeter.password = passwordTextfield.text
+                
+                sweeter.signUpInBackgroundWithBlock{
+                    (success:Bool!, error:NSError!)->Void in
+                    if error == nil{
+                        println("Sign Up successfull")
+                    }else{
+                        let errorString = error.localizedDescription
+                        println(errorString)
+                    }
+                    
+                    
+                }
+                
+                
+                
+            }))
+            
+            self.presentViewController(loginAlert, animated: true, completion: nil)
+        }
     }
-    
-    
+
     @IBAction func post(sender: UIButton) {
         var message : PFObject = PFObject(className: "Messages")
         message["content"] = messageBox.text
         message.saveInBackground()
+        //save textfield value
         self.view.center = originalCenter
+        //rreturn the view to regular view
         messageBox.resignFirstResponder()
+        //close keyboard
         messageBox.text = ""
+        //reset textfield
         
     }
     
@@ -45,11 +113,11 @@ class ThreadTableViewController: UIViewController, UITextViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     func textViewDidBeginEditing(textView: UITextView) {
-//        let keyHeight : CGFloat =
-//        CGFloat height = [[notification.userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].height;
+        //        let keyHeight : CGFloat =
+        //        CGFloat height = [[notification.userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].height;
         self.view.center = CGPointMake(self.originalCenter.x, self.originalCenter.y-216)
         println("HEY")
     }
