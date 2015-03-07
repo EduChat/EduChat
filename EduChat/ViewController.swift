07e3f1c07486c210f0a521adfe965bb7ec37670d
Eric Martin <ericmark26@gmail.com>
2015-03-07 10:31:48 -0500

    Added Elements

--- EduChat/ViewController.swift
+++ EduChat/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageBox: UITextView!
+    
+    override func viewDidAppear() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
     }
-
+    
+//    @IBAction func newPost(sender: UIButton) {
+//        
+//    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
